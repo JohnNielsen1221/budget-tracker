a6b20078fb88ecde62d6a635c1d95e3436746aed
John Nielsen <johnhayesnielsen@gmail.com>
2020-09-04 11:19:33 -0600

    attempt to add Atlas

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config()
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = `mongodb+srv://John_Nielsen1221:${process.env.DB_PASS}@cluster0.ooxcd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
